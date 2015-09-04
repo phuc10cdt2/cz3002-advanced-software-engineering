f71a3c480d9d4134c3014635998676a8114e310c
Phuc Nguyen <phuc10cdt2@gmail.com>
2015-09-04 10:43:44 +0800

    Added anonymous feature

--- controllers/RantController.js
+++ controllers/RantController.js
@@ -6,15 +6,17 @@ exports.create = function(req, res, next){
 	var owner = user.username;
 	var ownername = user.displayname;
 	//these values not set over yet
-	var lifetime = body.lifetime;
-	var viewtime = body.viewtime;
+	var lifetime = parseInt(body.lifetime,10);
+	var viewtime = parseInt(body.viewtime,10);
 	if(body.anonymous){
 		ownername = "Anonymous";
 	}
 	var rant = new Rant({
 		owner: owner,
 		content: body.content,
-		ownername: ownername
+		ownername: ownername,
+		lifetime:lifetime,
+		viewtime:viewtime
 	});
 	rant.save(function(err, rant, num){
 		if(err){
